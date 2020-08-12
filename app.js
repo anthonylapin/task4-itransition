fbd595153f22e4749dd02e2268192123b1bde65a
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-08-12 23:37:54 +0300

    i13

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
-const config = require('config')
 const path = require('path')
 
 const app = express()
 app.use(express.json({ extended: true }))
 
-app.use(express.static(path.join(__dirname, 'client', 'build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+}
+
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+// })
 
 app.use(morgan('tiny'))
 app.use('/api/auth', require('./routes/auth.routes'))
