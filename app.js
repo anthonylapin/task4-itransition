d88011ca1ba3cb339bfada432438f668bc95726b
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-08-12 21:51:50 +0300

    initial2

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 const config = require('config')
 const path = require('path')
 
 const app = express()
 app.use(express.json({ extended: true }))
 
+app.use(morgan('tiny'))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/content', require('./routes/content.routes'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    // })
 }
 
 let port = process.env.PORT || 5000
@@ -24,10 +26,14 @@ const MONGO_URI = config.get('mongoUri')
 
 const start = async () => {
     try {
-        await mongoose.connect(MONGO_URI, {
+        await mongoose.connect(process.env.MONGODB_URI || MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
+        })
+
+        mongoose.connection.on('connected', () => {
+            console.log('Mongoose is connected!');
         })
 
         app.listen(port, () => {
@@ -39,17 +45,17 @@ const start = async () => {
     }
 }
 
-process.on('uncaughtException', function (err) {
-    console.log(" UNCAUGHT EXCEPTION ")
-    console.log("[Inside 'uncaughtException' event] " + err.stack || err.message)
-    process.exit(0)
-})
+// process.on('uncaughtException', function (err) {
+//     console.log(" UNCAUGHT EXCEPTION ")
+//     console.log("[Inside 'uncaughtException' event] " + err.stack || err.message)
+//     process.exit(0)
+// })
 
-process.on('SIGTERM', function () {
-    app.close(function () {
-        process.exit(0);
-    })
-})
+// process.on('SIGTERM', function () {
+//     app.close(function () {
+//         process.exit(0);
+//     })
+// })
 
 start()
 
